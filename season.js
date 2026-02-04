1b1a5f779e9064eada5f3a7f5aca224f85e21c71
Boinichandu <2303a51737@sru.edu.in>
2026-02-04 15:20:36 +0530

    Add files via upload

---
+++ season.js
@@ -0,0 +1,17 @@
+function springImg(){
+    document.getElementById("backgroundimgSmall").src="https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+    document.getElementById("backgroundimgMid").src="https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png";
+}
+function summerImg(){
+    document.getElementById("backgroundimgSmall").src="https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png";
+    document.getElementById("backgroundimgMid").src="https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png";
+}
+function autumnImg(){
+    document.getElementById("backgroundimgSmall").src="https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png";
+    document.getElementById("backgroundimgMid").src="https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png";
+}
+function winterImg(){
+    document.getElementById("backgroundimgSmall").src="https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png";
+    document.getElementById("backgroundimgMid").src="https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png";
+}
+
