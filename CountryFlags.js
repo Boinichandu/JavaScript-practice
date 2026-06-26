3fe3330f9794d68152c26f5cd9866975339ae764
BoiniChandu <2303a51737@sru.edu.in>
2026-06-26 09:30:58 +0530

    Add files via upload

---
+++ CountryFlags.js
@@ -0,0 +1,61 @@
+let inputEl=document.getElementById('searchInput');
+let rowContainerEl=document.getElementById('rowcontainer');
+let countryCardEl1=document.createElement('div');
+rowContainerEl.appendChild(countryCardEl1);
+
+function createCards(items){
+    let countryCardEl=document.createElement('div');
+    countryCardEl.classList.add('d-flex','flex-row','justify-content-start','country-card');
+    
+    
+    let {name,flag,population}=items;
+    let imagecontainer=document.createElement('div');
+    
+    let imageEl=document.createElement('img');
+    imageEl.src=flag;
+    imageEl.classList.add('country-flag', 'p-1');
+    imagecontainer.appendChild(imageEl);
+    countryCardEl.appendChild(imagecontainer);
+    
+    let infoContainer=document.createElement('div');
+    let heading=document.createElement('h1');
+    heading.classList.add('country-name');
+    heading.textContent=name;
+    infoContainer.appendChild(heading);
+    
+    let countryNameEl=document.createElement('p');
+    countryNameEl.classList.add('country-population');
+    countryNameEl.textContent=population;
+    infoContainer.appendChild(countryNameEl);
+    
+    countryCardEl.appendChild(infoContainer);
+    countryCardEl1.appendChild(countryCardEl);
+    
+} 
+inputEl.addEventListener('keydown',function(each){
+    let option={
+        method:'GET'
+    }
+    
+    if(each.key==='Enter'){
+        countryCardEl1.textContent="";
+        let countrysearched=inputEl.value;
+        fetch("https://apis.ccbp.in/countries-data",option)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            if(countrysearched.trim()!==''){
+                for(let item of data){
+                    if(item.name.includes(countrysearched)){
+                        createCards(item);
+                    }
+                }
+            }else{
+                 for(let item of data){
+                    createCards(item);
+                }
+            }
+        })
+    }
+})
